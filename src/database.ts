535671ec5a83569e92642b3142829c47b2974620
Don2Quixote <idonquixote@yandex.ru>
2020-11-23 16:42:01 +0300

    Added method to get sesion data states

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { Connection as sqlConnection } from 'mysql2';
 
 /*
  sessionStates:
- datetime, id, currency, card, depositAddressId, depositAddress, depositAmount, orderId, exchanged, fundsReceived
+ datetime, id, status, currency, card, withdrawMethod, withdrawMethodFee, depositAddressId, depositAddress, depositAmount, orderId, exchanged, fundsReceived, withdrawId, withdrawSucceed
  */
 
 function convert(value): string {
@@ -16,17 +16,22 @@ function convert(value): string {
 
 export function addSessionDataState(db: sqlConnection, data) {
     return new Promise((resolve, reject) => {
-        let query = 'INERT INTO `sessionStates` VALUES (now(), ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        let query = 'INERT INTO `sessionStates` VALUES (now(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
         db.query(query, [
             convert(data.id),
+            convert(data.status),
             convert(data.currency),
             convert(data.card),
+            convert(data.withdrawMethod),
+            convert(data.withdrawMethodFee),
             convert(data.depositAddressId),
             convert(data.depositAddress),
             convert(data.depositAmount),
             convert(data.orderId),
             convert(data.exchanged),
-            convert(data.fundsRecieved)
+            convert(data.fundsReceived),
+            convert(data.withdrawId),
+            convert(data.withdrawSucceed)
         ], (err, result) => {
             if (err) reject(err);
             else resolve(result);
@@ -34,3 +39,32 @@ export function addSessionDataState(db: sqlConnection, data) {
     });
 }
 
+export function getSessionDataStates(db: sqlConnection, sessionId: string) {
+    return new Promise((resolve, reject) => {
+        let query = 'SELECT * from `sessionStates`';
+        db.query(query, (err, result: any) => {
+            if (err) reject(err);
+            else {let states = [];
+                for (let state of result) {
+                    states.push({
+                        id: state.id,
+                        status: state.status,
+                        currency: state.currency,
+                        card: state.card,
+                        withdrawMethod: state.withdrawMethod,
+                        withdrawMethodFee: state.withdrawMethodFee,
+                        depositAddressId: state.depositAddressId,
+                        depositAddress: state.depositAddress,
+                        depositAmount: state.depositAmount,
+                        orderId: state.orderId,
+                        exchanged: state.exchanged,
+                        fundsReceived: state.fundsReceived,
+                        withdrawId: state.withdrawId,
+                        withdrawSucceed: state.withdrawSucceed,
+                    });
+                }
+                resolve(states);
+            }
+        });
+    });
+}
